ee7bf607b7c32c0c2ecb9cc606aae46cb1d54136
anshuraj1996 <anshumansingh355@gmail.com>
2022-05-16 07:20:23 +0530

    done

---
+++ front_task1/my-app/src/App.js
@@ -0,0 +1,15 @@
+
+// import './App.css';
+import TreeList from "./Components/users"
+
+function App() {
+  return (
+    <div className="App">
+     
+      <TreeList/>
+     
+    </div>
+  );
+}
+
+export default App;
